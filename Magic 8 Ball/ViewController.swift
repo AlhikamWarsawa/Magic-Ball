4a78fe0512ad90e5853b9a1f251bfb795573eafc
AlhikamWarsawa <alhikam200@gmail.com>
2025-03-01 17:51:37 +0700

    Third Project

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    @IBOutlet weak var wishBall: UIImageView!
+    
+    var ball = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wishBall.alpha = 0.5
+    }
+    // Button
+    
+    @IBAction func askButton(_ sender: Any) {
+        wishBall.alpha = 1
+        
+        let spinBall = [
+            UIImage(named: "ball1"),
+            UIImage(named: "ball2"),
+            UIImage(named: "ball3"),
+            UIImage(named: "ball4"),
+            UIImage(named: "ball5")
+        ]
+        
+        let randomBall = Int.random(in: 0...4)
+        
+        wishBall.image = spinBall[randomBall]
+    }
 }
 
